0b353885f62131610388e988c1b67aa978a7196a
Brahim Nedjar <skullneed@gmail.com>
2023-09-13 20:42:53 +0100

    splash logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/features/Splash/Presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -11,8 +13,10 @@ class BooklyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SplashView()
+    return  GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(scaffoldBackgroundColor: kColor),
+      home: const SplashView()
     );
   }
 }
