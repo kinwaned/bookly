00d516ae18cdb3db5dc8042625d09281e0b54873
Brahim Nedjar <skullneed@gmail.com>
2023-09-14 23:43:20 +0100

    custom app bar added

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -9,8 +10,32 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
+        CustomListViewItem(),
       ],
     );
   }
 }
 
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .28,
+      child: AspectRatio(
+        aspectRatio: 2.7/4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.red,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(AssetsData.testBook1),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
