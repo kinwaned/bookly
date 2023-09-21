cea752c543531473a677c34b6db1a2f3e5a60ffc
Brahim Nedjar <skullneed@gmail.com>
2023-09-21 19:59:12 +0100

    update

--- lib/features/home/presentation/views/widgets/custom_list_view.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view.dart
@@ -21,3 +21,5 @@ class FeaturedListViewItem extends StatelessWidget {
     );
   }
 }
+
+
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/assets.dart';
 import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -16,11 +17,49 @@ class HomeViewBody extends StatelessWidget {
         children: [
           CustomAppBar(),
           FeaturedBookListView(),
-          SizedBox(height: 50,),
-          Text('Best Sellers', style: Styles.titleMedium,),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Best Sellers',
+            style: Styles.titleMedium,
+          ),
+          BestSellersListViewItem()
         ],
       ),
     );
   }
 }
 
+class BestSellersListViewItem extends StatelessWidget {
+  const BestSellersListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                // color: Colors.red,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testBook2),
+                ),
+              ),
+            ),
+          ),
+          const Column(
+            children: [
+
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
