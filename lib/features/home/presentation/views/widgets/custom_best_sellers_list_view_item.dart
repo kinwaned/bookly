72545e9becf9851e9eec7b87d606723ba477650d
Brahim Nedjar <skullneed@gmail.com>
2023-09-22 21:50:45 +0100

    best sellers item finished

--- lib/features/home/presentation/views/widgets/custom_best_sellers_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_best_sellers_list_view_item.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../constants.dart';
@@ -29,23 +30,31 @@ class BestSellersListViewItem extends StatelessWidget {
           const SizedBox(
             width: 30,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child:  Text(
-                  'Harry Potter And the Goblet of Fire',
-                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectra),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child:  Text(
+                    'Harry Potter And the Goblet of Fire',
+                    style: Styles.textStyle20.copyWith(fontFamily: kGtSectra),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 3,),
-              Text('J.K Rowling', style: Styles.textStyle14.copyWith(color: Colors.grey),),
-              const SizedBox(height: 3,),
-              Text('19.99€', style: Styles.textStyle20.copyWith(fontFamily: kMontserrat, fontWeight: FontWeight.bold),),
-            ],
+                const SizedBox(height: 3,),
+                Text('J.K Rowling', style: Styles.textStyle14.copyWith(color: const Color(0xff707070)),),
+                const SizedBox(height: 3,),
+                Row(
+                  children: [
+                    Text('19.99€', style: Styles.textStyle20.copyWith(fontFamily: kMontserrat, fontWeight: FontWeight.bold),),
+                    const Spacer(),
+                    const BookRating(),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -34,3 +34,5 @@ class HomeViewBody extends StatelessWidget {
 }
 
 
+
+
