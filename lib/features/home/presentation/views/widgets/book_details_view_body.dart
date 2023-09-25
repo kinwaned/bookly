f1f245c3349832a9e4162c4859b974d6c70194ca
Brahim Nedjar <skullneed@gmail.com>
2023-09-25 22:37:22 +0100

    custom scroll view added

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -12,68 +12,74 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: CustomBookDetailsAppBar(),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: width * .27),
-          child: CustomBookImage(
-            borderRadius: BorderRadius.circular(20),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              const SizedBox(height: 16),
-              const Text(
-                'Harry Potter And the Goblet of Fire',
-                style: Styles.textStyle26,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 6),
-              Opacity(
-                opacity: .7,
-                child: Text(
-                  'J.K Rowling',
-                  style: Styles.textStyle18.copyWith(
-                      fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              const SizedBox(height: 16),
-              const BookRating(
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-              const SizedBox(height: 37),
-              const BooksAction(),
-              const SizedBox(
-                height: 50,
-              ),
-               Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'You may also like',
-                  style: Styles.textStyle16.copyWith(
-                    fontFamily: kMontserrat,
-                    fontWeight: FontWeight.bold
+    var width = MediaQuery
+        .of(context)
+        .size
+        .width;
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                CustomBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * .27),
+                  child: CustomBookImage(
+                    borderRadius: BorderRadius.circular(20),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-            ],
+                const SizedBox(height: 16),
+                const Text(
+                  'Harry Potter And the Goblet of Fire',
+                  style: Styles.textStyle26,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 6),
+                Opacity(
+                  opacity: .7,
+                  child: Text(
+                    'J.K Rowling',
+                    style: Styles.textStyle18.copyWith(
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w500),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(height: 37),
+                const BooksAction(),
+                const Expanded(
+                  child: SizedBox(
+                    height: 50,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You may also like',
+                    style: Styles.textStyle16.copyWith(
+                        fontFamily: kMontserrat,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SimilarBooksListView(),
+                const SizedBox(
+                  height: 40,
+                ),
+
+              ],
+            ),
           ),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(left: 20),
-          child: SimilarBooksListView(),
         ),
       ],
     );
