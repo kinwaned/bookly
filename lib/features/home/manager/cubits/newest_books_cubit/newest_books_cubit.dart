0bd0fb9f338680956a743af4aa15583df02cc288
Brahim Nedjar <skullneed@gmail.com>
2023-09-29 20:10:06 +0100

    Newest cubit method created

---
+++ lib/features/home/manager/cubits/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../../../data/model/book_model/book_model.dart';
+import '../../../data/repos/home_repos.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(NewestBooksFailure(failure.errorMessage));
+    } , (books) {
+      emit(NewestBooksSuccess(books));
+    } );
+  }
+}
