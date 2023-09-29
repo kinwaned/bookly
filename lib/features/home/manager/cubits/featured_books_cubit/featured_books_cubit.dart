3e46f689219333d1662eb54dd725b414935df62f
Brahim Nedjar <skullneed@gmail.com>
2023-09-29 19:48:18 +0100

    Featured cubit method created

--- lib/features/home/manager/cubits/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/manager/cubits/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/data/repos/home_repos.dart';
 import 'package:meta/meta.dart';
 import '../../../data/model/book_model/book_model.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
-}
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(failure.errorMessage));
+    } , (books) {
+      emit(FeaturedBooksSuccess(books));
+    } );
+  }
+ }
