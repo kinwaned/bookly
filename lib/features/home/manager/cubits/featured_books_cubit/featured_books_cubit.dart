2a16f371de40c65c12ecfe1461f24be18bb23dd4
Brahim Nedjar <skullneed@gmail.com>
2023-09-29 19:40:05 +0100

    Featured cubit created

---
+++ lib/features/home/manager/cubits/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import '../../../data/model/book_model/book_model.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
